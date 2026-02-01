31ae5d763036a48fed9f7b18d48f1130e5064840
Shreya Sharma <shreyasharm29@gmail.com>
2026-02-01 23:39:23 +0530

    Create range and function example script
    Added a script that demonstrates range usage and defines a function to add two numbers.

---
+++ range and function.py
@@ -0,0 +1,17 @@
+#range funtion 
+for i in range (9,30):
+    print (i,end=" ")
+
+# range funtion print no in gaps 
+for i in range (2,33,3):
+    print(i,end=" ")
+
+# funtions 
+
+def addTwonumbers(a,b):
+    sum= a+b
+    return sum
+    
+ans = addTwonumbers(4,7)
+print(ans)
+    
