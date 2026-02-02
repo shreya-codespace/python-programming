9b3c44b9c63b7b450ece1678f410b27b3a0ebf32
Shreya Sharma <shreyasharm29@gmail.com>
2026-02-02 20:15:21 +0530

    Add basic Python script for learning purposes
    This script demonstrates basic Python syntax, including variable assignment, conditional statements, and user input.

---
+++ basic of python.py
@@ -0,0 +1,46 @@
+print ("hello world ")
+
+monday = 90
+tuesday = 100
+avg = (monday +tuesday)/2
+print (avg)
+
+s1 = 's'
+s2 = "shreya"
+s3 =  "my name is shreya"
+print(s3,type(s3))
+print(s2,type(s2))
+print(s1,type(s1))
+
+ismarried = False
+isgraduated = True 
+print(ismarried, type(False))
+print(isgraduated, type(True))
+
+first_name = input()
+last_name = input()
+print(first_name)
+print(last_name)
+
+age = int (input())
+height = float (input())
+print(age,height)
+
+age = int (input())
+if (age>=18):
+    test = input() 
+    if test == "PASS":
+       print("eligible for licence ")
+    else:
+        print("not eligible for licence")
+else:     
+        print("not eligible for licence")
+
+elif marks >=70:
+  print("good")
+elif marks >=40:
+  print ("fair")
+else:
+  print ("bad")
+   
+
