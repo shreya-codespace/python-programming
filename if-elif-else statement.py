13055f4229702fef17cbb2cb78c4b5010576c8b5
Shreya Sharma <shreyasharm29@gmail.com>
2026-03-12 15:11:25 +0530

    Implement number sign checking with if-elif-else
    Add if-elif-else structure to check number sign

---
+++ if-elif-else statement.py
@@ -0,0 +1,10 @@
+num = int(input("enter the value of num:"))
+if num < 0:
+    print("number is negative")
+elif num==0:
+    print("number is zero")
+else:
+    print("number is positive")
+    
+print("i am soooo happpy")
+    
