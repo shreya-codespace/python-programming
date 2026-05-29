f4d823f5266e98343609bc72918d884af3abba57
Shreya Sharma <shreyasharm29@gmail.com>
2026-05-29 12:21:59 +0530

    Add arithmetic, assignment, and comparison operators examples

---
+++ assignment and comparison operator.py
@@ -0,0 +1,25 @@
+num1= 10
+num2= 20
+
+#arithemetic operators
+print("num1+num2",num1+num2)
+print("num1-num2",num1-num2)
+print("num1*num2",num1*num2)
+print("num1/num2",num1/num2)
+print("num1//num2",num1//num2)#double float
+print("num1**num2",num1**num2)#exponential
+print("num1%num2",num1%num2)#remainder after divide
+
+#assignment operators
+a =5
+a += 2# same for -,*,/,//,**,%
+print(a)
+
+#comparison operators
+x =3
+y =4
+print(x>y)
+print(y>x)
+print(x!=y)
+print(x==y)
+
