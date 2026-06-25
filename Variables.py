640aa6768ab9eb0982997ded8c00513e5d467109
Shreya Sharma <shreyasharm29@gmail.com>
2026-06-25 17:52:12 +0530

    Add variable examples and naming rules in Variables.py
    Added variable declarations and print statements demonstrating variable naming rules in Python.

---
+++ Variables.py
@@ -0,0 +1,8 @@
+_shreya = "a beautiful girl" #  we can't  start  a variable name with a number use underscore instead
+print (_shreya)
+# varible can contain letters ,digit  ,underscore but can't start with a digit
+# cannot use python keywords (if, else, while, for, break, continue, return, def, class, import, from, as, pass, try, except, finally, with, lambda, yield, global, nonlocal, assert, del) as variable names
+age = "20"
+Age = "40"
+AGE = "30" # python is case sensitive so age and AGE are different variables
+print (age,Age,AGE)
