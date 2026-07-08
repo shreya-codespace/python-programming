908332ffad26e09d15984991e97e8eb221dba7bb
Shreya Sharma <shreyasharm29@gmail.com>
2026-07-08 23:13:38 +0530

    Create  python operators.py

---
+++ python operators.py
@@ -0,0 +1,54 @@
+# arthemetic operators 
+a = 10
+b = 5
+c = a + b # addition
+d = a - b # subtraction
+e = a * b # multiplication
+f = a / b # division
+g = a % b # modulus
+h = a ** b # exponentiation
+
+# assignment operators
+x = 4-2 # assignn 4-2 in a 
+print (x)
+a =5
+a -=2# decrement the value of a by 2 and then assign it to a
+print(a)
+
+#comparison operators it always return boolean value either True or False
+d = 5<4
+print(d) # less than operator
+
+d = 5>4
+print(d) # greater than operator
+
+d = 5<=4
+print(d) # less than or equal to operator
+
+d = 5>=4
+print(d) # greater than or equal to operator
+
+d = 5==4
+print(d) # equal to operator
+
+d = 5!=4
+print(d) # not equal to operator
+
+#logical operators
+a = True or False 
+print(a) # or operator
+# Truth table for or operator
+# True or True = True
+# True or False = True
+# False or True = True
+# False or False = False
+
+# Truth table for and operator
+# True and True = True
+# True and False = False    
+# False and True = False
+# False and False = False
+
+#print not operator
+print(not (False))
+print(not (True))
