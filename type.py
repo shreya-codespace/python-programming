b2868995bb02a0d45a56b80d76d2660a967d7f8e
Shreya Sharma <shreyasharm29@gmail.com>
2026-07-08 23:12:01 +0530

    Add type.py to showcase Python data types
    Demonstrate type checking for various data types in Python.

---
+++ type.py
@@ -0,0 +1,40 @@
+A = 22
+type(A)
+print(type(A))  # <class 'int'>
+
+b= "shreya"
+print(type(b))  # <class 'str'>
+
+c= 3.5
+print(type(c))  # <class 'float'>
+
+d= True
+print(type(d))  # <class 'bool'>
+
+e= [1, 2, 3, 4]
+print(type(e))  # <class 'list'>    
+
+f= (1, 2, 3, 4)
+print(type(f))  # <class 'tuple'>
+
+g= {"name": "shreya", "age": 20}
+print(type(g))  # <class 'dict'>    
+
+h= {1, 2, 3, 4}
+print(type(h))  # <class 'set'>
+
+i= None
+print(type(i))  # <class 'NoneType'>
+
+j="4.4"
+print(type(j))
+
+a= "31.2"
+b= float(a)
+t = type(b)
+print(t)
+
+c=4
+d=str(c)
+t=type(d)
+print(t)
