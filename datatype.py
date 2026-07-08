228c5444165ea9945e969c51940d325b5513ef57
Shreya Sharma <shreyasharm29@gmail.com>
2026-07-08 23:12:38 +0530

    Create datatype.py

---
+++ datatype.py
@@ -0,0 +1,17 @@
+a= 1# a is an integer variable
+
+b= 5.23 # b is a float variable
+
+c= "shreya" # c is a string variable
+
+d= True # d is a boolean variable
+
+e= [1, 2, 3, 4] # e is a list variable
+
+f= (1, 2, 3, 4) # f is a tuple variable
+
+g= {"name": "shreya", "age": 20} # g is a dictionary variable
+
+h= {1, 2, 3, 4} # h is a set variable
+
+i= None # i is a NoneType variable
