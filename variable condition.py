b9b8f1b27b7e9590511caa028e01460de2f19c8f
Shreya Sharma <shreyasharm29@gmail.com>
2026-07-09 22:23:06 +0530

    Create variable condition.py

---
+++ variable condition.py
@@ -0,0 +1,6 @@
+# a variable name can contain letters, numbers, and underscores. A variable name cannot start with a number. A variable name cannot contain spaces. A variable name cannot be a reserved word in Python. A variable name is case-sensitive.
+# a variable is a container that holds a value. A variable can hold different types of values, such as integers, floats, strings, booleans, lists, tuples, dictionaries, sets, and NoneType. A variable can be assigned a value using the assignment operator (=). A variable can be reassigned a new value at any time. A variable can be deleted using the del statement.
+# a variable name can only starts with a letter or an underscore.
+# no while space is allowed to be used inside a variable name.
+
+
